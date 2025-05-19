f18b7ccb57b7d09ac4e2a5ff4977e4e2f3b8bf82
Luke Quinane <luke.quinane@gmail.com>
2025-05-19 21:58:17 +1000

    Filter out negative values

--- custom_components/nsw_air_quality/sensor.py
+++ custom_components/nsw_air_quality/sensor.py
@@ -113,3 +113,7 @@ class AirQualitySensor(SensorEntity):
         previous_hour = (datetime.now() - timedelta(hours = 1)).hour
         entry = next((item for item in sensor_data if item["Hour"] == previous_hour), None)
         self._attr_native_value = entry.get("Value")
+
+        if self._attr_native_value < 0:
+            _LOGGER.warning("Negative value for sensor '%s': %f", self._attr_name, entry.get("Value"))
+            self._attr_native_value = 0
