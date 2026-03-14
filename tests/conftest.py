6e23f37c82dc51cf109ca13e65ccff8a62fdabb5
Luke Quinane <luke.quinane@gmail.com>
2026-03-14 22:17:02 +1100

    Formatting fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 """Test configuration and fixtures."""
+
 import sys
 import os
 from unittest.mock import MagicMock
@@ -15,18 +16,18 @@ except ImportError:
     _ha = MagicMock()
     _ha.util.Throttle = lambda min_time: lambda f: f
     _ha_modules = {
-        'homeassistant': _ha,
-        'homeassistant.util': _ha.util,
-        'homeassistant.config_entries': _ha.config_entries,
-        'homeassistant.core': _ha.core,
-        'homeassistant.exceptions': _ha.exceptions,
-        'homeassistant.const': _ha.const,
-        'homeassistant.helpers': _ha.helpers,
-        'homeassistant.helpers.typing': _ha.helpers.typing,
-        'homeassistant.helpers.entity': _ha.helpers.entity,
-        'homeassistant.helpers.entity_platform': _ha.helpers.entity_platform,
-        'homeassistant.helpers.device_registry': _ha.helpers.device_registry,
-        'homeassistant.components': _ha.components,
-        'homeassistant.components.sensor': _ha.components.sensor,
+        "homeassistant": _ha,
+        "homeassistant.util": _ha.util,
+        "homeassistant.config_entries": _ha.config_entries,
+        "homeassistant.core": _ha.core,
+        "homeassistant.exceptions": _ha.exceptions,
+        "homeassistant.const": _ha.const,
+        "homeassistant.helpers": _ha.helpers,
+        "homeassistant.helpers.typing": _ha.helpers.typing,
+        "homeassistant.helpers.entity": _ha.helpers.entity,
+        "homeassistant.helpers.entity_platform": _ha.helpers.entity_platform,
+        "homeassistant.helpers.device_registry": _ha.helpers.device_registry,
+        "homeassistant.components": _ha.components,
+        "homeassistant.components.sensor": _ha.components.sensor,
     }
     sys.modules.update(_ha_modules)
